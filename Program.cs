da8743cfdd6a1ad7cd2a85e79265481b596318ae
deliacirstea <deliutacirstea@gmail.com>
2022-10-18 20:49:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, stranger!\n");
+
+Console.WriteLine("Welcome to my 3rd C# challenge!");
+Console.WriteLine("**************************************************\n");
+
+Console.WriteLine("“The story of life is quicker than the wink of an eye, the story of love is hello and goodbye...until we meet again”\r\n― Jimi Hendrix");
+
+
+
+
